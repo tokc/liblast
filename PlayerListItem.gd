42fa5bae1b1c3c402d971b638c3f94a55368b199
tokc <eviloatmeal@eviloatmeal.com>
2021-03-30 22:33:58 +0200

    Add labels for kills and deaths to scoreboard.

--- PlayerListItem.gd
+++ PlayerListItem.gd
@@ -1,8 +1,13 @@
 extends HBoxContainer
 
 var player setget set_player
+var kills: int = 0
+var deaths: int = 0
 var network_id
 
 func set_player(_player):
 	$Nickname.text = _player.nickname
+	$Kills.text = str(kills)
+	$Deaths.text = str(deaths)
 	network_id = _player.name
+
